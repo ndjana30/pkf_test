eb386899ea3ae607a4a6843ef447322e026fd8bb
ndjana30 <davyndjana@gmail.com>
2026-05-22 16:22:41 +0100

    offline code join to repo

---
+++ src/main/java/com/ndjana/pkf/PkfApplication.java
@@ -0,0 +1,13 @@
+package com.ndjana.pkf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ndjana/pkf/PkfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ndjana.pkf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
